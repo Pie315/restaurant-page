85eaa96bdd0182ca63617a4126affa2bacab7005
Pie315 <devonkaelberer@gmail.com>
2023-06-17 11:21:46 -0400

    page building code started

--- src/home.js
+++ src/home.js
@@ -1,7 +1,20 @@
+import ingredients from './Ingredients.png'
+
+
+
 function buildLanding() {
+
+
+    const image = new Image();
+    image.src = ingredients
+
+    image.classList.add("image")
+
     const container = document.querySelector("#content")
     container.textContent = "TESTTEST"
     console.log("Homerun")
+
+    container.appendChild(image)
 
     
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import './style.css';
 import buildLanding from './home';
 
+const buttonMaker = (selected) => {
+
+    const content = document.querySelector("#content");
+    const box = document.createElement("div");
+    box.classList.add([".flexy", ".box"]);
+    const home = document.createElement("button");
+    const menu = document.createElement("button");
+    const contact = document.createElement("button");
+
+    home.textContent = "home"
+    menu.textContent = "menu"
+    contact.textContent = "contact"
+
+    const tabs = [home, menu, contact];
+    tabs.forEach((tab) => {
+        box.appendChild(tab)
+    })
+
+    switch(selected) {
+        case "home":
+            home.classList.add("current")
+            break
+        case "menu":
+            menu.classList.add("current")
+            break
+        case "contact":
+            contact.classList.add("current")
+            break
+        default:
+            console.log("error");
+    }
+    content.appendChild(box);
+}
+
+
 console.log("TESTWATCH");
 
+buttonMaker()
 buildLanding();
