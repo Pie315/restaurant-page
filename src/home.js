fc6156daa265d7acd3c67c40126498a977d467a9
Pie315 <devonkaelberer@gmail.com>
2023-06-19 12:52:36 -0400

    added content to homepage

--- src/home.js
+++ src/home.js
@@ -11,6 +11,14 @@ function buildHome() {
     image.src = ingredients
     image.classList.add("image")
 
+    const blurb = document.createElement("h2")
+    blurb.textContent = "About us"
+    pageInner.appendChild(blurb)
+
+    const message = document.createElement("p")
+    message.textContent = "Here at Hollywood Diners we pride ourselves on maintaining the highest quality customer service. Our food is crafted with the highest quality GMO ingredients, and we cater to our customer's need such as vegan and gluten free options"
+    pageInner.appendChild(message)
+
 
     pageInner.appendChild(image)
 
